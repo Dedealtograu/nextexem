99ab461efc1add93215c4beb32375bb95b6b1e6c
Dedealtograu <matias.jms14@gmail.com>
2024-12-19 10:58:41 -0300

    Exemple by Dedealtograu

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,55 @@
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import { AppType } from 'next/dist/shared/lib/utils'
+import { JSX } from 'react'
+
+interface MyDocumentProps extends DocumentInitialProps {
+  styles: JSX.Element
+}
+
+export default class MyDocument extends Document<MyDocumentProps> {
+  static async getInitialProps(ctx: DocumentContext): Promise<MyDocumentProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: AppType) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
